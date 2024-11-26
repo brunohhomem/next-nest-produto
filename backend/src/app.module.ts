256434cf2b75b4381bec8bddb2f6f4b71e52058e
Bruno H. Homem <brunohhomem@gmail.com>
2024-11-26 20:52:14 -0300

    feat: create CRUD Produto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { DbModule } from './db/db.module';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DbModule, ProdutoModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ backend/src/produto/produto.service.ts
@@ -0,0 +1,42 @@
+import { PrismaService } from './../db/prisma.service';
+import { Injectable } from '@nestjs/common';
+import { CreateProdutoDto } from './dto/create-produto.dto';
+import { UpdateProdutoDto } from './dto/update-produto.dto';
+
+@Injectable()
+export class ProdutoService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createProdutoDto: CreateProdutoDto) {
+    return this.prismaService.produto.create({
+      data: createProdutoDto,
+    });
+  }
+
+  findAll() {
+    return this.prismaService.produto.findMany({
+      orderBy: {
+        id: 'asc',
+      },
+    });
+  }
+
+  findOne(id: number) {
+    return this.prismaService.produto.findUnique({
+      where: { id }, //Condição do findUnique
+    });
+  }
+
+  update(id: number, updateProdutoDto: UpdateProdutoDto) {
+    return this.prismaService.produto.update({
+      where: { id },
+      data: updateProdutoDto,
+    });
+  }
+
+  remove(id: number) {
+    return this.prismaService.produto.delete({
+      where: { id },
+    });
+  }
+}
